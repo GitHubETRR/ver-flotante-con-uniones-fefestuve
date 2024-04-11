c10d8e4cee9119dd683ebfd9226876ba3de7ef4d
fefestuve <123496126+fefestuve@users.noreply.github.com>
2024-04-11 12:07:59 -0300

    Update UnionFloats.c

--- UnionFloats.c
+++ UnionFloats.c
@@ -9,9 +9,8 @@ int main(void){
     union numeros numero;
     printf("Ingresa un numero \n");
     scanf("%f",&numero.var);
-    printf("El numero que ingresaste esta guardado en la memoria como ");
+    printf("El numero que ingresaste esta guardado en la memoria como 0x");
     for (int i = 3; i >= 0; i--){
-        printf("%x",numero.vect[i]);
+        printf("%02X",numero.vect[i]);
     }
-    printf(" en hexadecimal\n");
 }
